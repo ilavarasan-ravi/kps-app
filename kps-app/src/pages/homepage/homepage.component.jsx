f89abbb5521c2210ad2497f72d82e228cb94cf8b
Ilavarasan Ravi <ilavarasan.ravi@gmail.com>
2020-04-22 00:21:19 -0700

    base design 1

--- kps-app/src/pages/homepage/homepage.component.jsx
+++ kps-app/src/pages/homepage/homepage.component.jsx
@@ -3,9 +3,9 @@ import './homepage.styles.scss'
 import Directory from '../../components/directory/directory.component'
 const HomePage = () => (
     <div className='homepage'>
+        <h1>Title</h1>
         <div className='directory-menu'>
             <Directory />
-
         </div>
     </div>
 )
